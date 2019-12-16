1560704bdfbcd9958597e291030f3aff4208cd3d
unknown <292796774@qq.com>
2019-12-16 16:09:52 +0800

    first commit

---
+++ app/src/main/java/com/study/intentyinshi/ViceActivity.java
@@ -0,0 +1,14 @@
+package com.study.intentyinshi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vice);
+    }
+}
